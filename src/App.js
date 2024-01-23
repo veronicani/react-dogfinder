a13314b5cd9b80f888740ba3c6fa6a58c4e6c2a4
Veronica Ni <vni1294@gmail.com>
2024-01-22 19:20:19 -0500

    Implementing useEffect to makefetch req

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import DogList from './DogList';
 import DogDetails from './DogDetails';
 import Nav from './Nav';
 
-async function App() {
-  const dogData = await makeRequest();
-  console.log("Dog data in App", dogData)
-  // const arrObj=[{a:"a", b:"b"}, {a:"a", b:"b"}]
+function App() {
+  const [dogData, setDogData] = useState(null);
 
+  useEffect(() => {
+    console.log("useEffect");
+    async function makeRequest() {
+      const response = await fetch(`http://localhost:5001/dogs`);
+      const dogData = await response.json();
+      console.log("dogData from fetch: ", dogData);
+      setDogData(dogData);
+    }
+    makeRequest();
+  }, []);
+
+  console.log('dogData state: ', dogData);
   return (
     <BrowserRouter>
-      <Nav dogs={1}/>
+      <Nav dogs={dogData}/>
       <Routes>
-        <Route element={<DogList dogs={1}/>} path="/dogs" />
+        <Route element={<DogList dogs={dogData}/>} path="/dogs" />
         <Route element={<DogDetails />} path="/dogs/:name" />
       </Routes>
     </BrowserRouter>
   );
 }
 
-async function makeRequest(){
-  const response = await fetch(`http://localhost:5001/dogs`);
-  const dogData = await response.json();
+// async function makeRequest(){
+//   const response = await fetch(`http://localhost:5001/dogs`);
+//   const dogData = await response.json();
 
-  console.log("DogData:", dogData)
-  return dogData;
-}
+//   console.log("DogData:", dogData)
+//   setDogData(dogData);
+// }
 
 export default App;
